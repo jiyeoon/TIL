59b7a108f70124c17fa3e7c5259f81840103e2a9
jiyeon <i3048i@naver.com>
2021-01-26 21:57:18 +0900

    Add 2019solution.py

---
+++ Algorithm/HashCode/2019solution.py
@@ -0,0 +1,74 @@
+
+
+import random
+import collections
+import numpy as np
+from tqdm import tqdm
+
+random.seed(42)
+
+REARRANGE_FOR_MERGE = True
+MERGE_WINDOW = 10000
+ARRANGE_WINDOW = 10000
+
+def read_file(file_path):
+    with open(filepath) as f:
+        pictures = [row.strip().split() for row in f.readlines()[1:]]
+    
+    pic_tags = {}
+    horizontal_photos = [] # horizontal photos only
+    vertical_photos = [] # vertical photos only
+    
+    for i, pictures in enumerate(pictures):
+        pic_tags[i] = set(pictures[2:]) # 태그들 넣음
+        if pictures[0] == "H":
+            horizontal_photos.append(i)
+        elif pictures[0] == "V":
+            vertical_photos.append(i)
+    
+    return pic_tags, horizontal_photos, vertical_photos
+
+file_path = './input/'
+pic_tags, horizontal_photos, vertical_photos = read_file(file_path)
+
+def calc_tags_pair_score(tags1, tags2):
+    return min(len(tags1 & tags2), len(tags1 - tags2), len(tags2 - tags1))
+
+
+def calc_idxs_pair_score(idxs1, idxs2):
+    # given two tuples of indices, calculate the score
+    return calc_tags_pair_score(
+        set.union(*[pic_tags[idx] for idx in idxs1]),
+        set.union(*[pic_tags[idx] for idx in idxs2]))
+
+def calc_idxs_pair_score_max(idxs1, idxs2):
+    # given two tuples of indices, calculate the maximum possible score by tag length
+    return min(len(set.union(*[pic_tags[idx] for idx in idxs1])),
+               len(set.union(*[pic_tags[idx] for idx in idxs2])))//2
+
+def calc_sequence(idxs_lst):
+    # given the sequence of indices, calculate the score
+    check_validity(idxs_lst)
+    score = 0
+    for before, after in zip(idxs_lst[:-1], idxs_lst[1:]):
+        score += calc_idxs_pair_score(before, after)            
+    return score
+
+def calc_sequence_max(idxs_lst):
+    # given the sequence of indices, calculate the score
+    check_validity(idxs_lst)
+    score = 0
+    for before, after in zip(idxs_lst[:-1], idxs_lst[1:]):
+        score += calc_idxs_pair_score_max(before, after)            
+    return score
+
+def check_validity(idxs_lst):
+    all_pics = [idx for idxs in idxs_lst for idx in idxs]
+    if len(all_pics) != len(set(all_pics)):
+        print("Duplicates found")
+    all_verts = [idx for idxs in idxs_lst for idx in idxs if len(idxs) == 2]
+    if (set(all_verts) - set(vertical_photos)):
+        print("Horizontal photos found in vertical combinations")
+    all_horis = [idx for idxs in idxs_lst for idx in idxs if len(idxs) == 1]
+    if (set(all_horis) - set(horizontal_photos)):
+        print("Vertical photos found in horizontal arrangement")
--- Hands_On_Machine_Learning/README.md
+++ Hands_On_Machine_Learning/README.md
@@ -6,3 +6,4 @@
 - [Chapter 3. 분류](./ch3_분류.ipynb)
 - [Chapter 4. 모델 훈련](./ch4_모델훈련.ipynb)
 - [Chapter 5. 서포트 벡터 머신](./ch5_서포트벡터머신.ipynb)
+- [Chapter 6. 결정 트리](./ch6_결정트리.ipynb)
