db987bd044748bdbf9c3bcf9eca92c24d4e45bf7
jiyeon <i3048i@naver.com>
2021-03-21 22:53:11 +0900

    Django Test..

--- django_test/demo/views.py
+++ django_test/demo/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def base(request):
     return render(request, 'demo/base.html', {})
+
+def sidebar(request):
+    return render(request, 'demo/base.html', {})
