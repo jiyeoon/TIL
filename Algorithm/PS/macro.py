aa79cf020b83c81ecfd349ce97f5e8b11ff4f358
Jiyeon Lee <i3048i@yonsei.ac.kr>
2021-06-23 23:53:22 +0900

    Update macro.py

--- Algorithm/PS/macro.py
+++ Algorithm/PS/macro.py
@@ -8,3 +8,64 @@ while True:
     pag.moveRel(n, 0)
     time.sleep(1)
 
+class ArcMarginProduct(tf.keras.layers.Layer):
+    def __init__(self, n_classes, s=30, m=0.50, easy_margin=False,
+                 ls_eps=0.0, **kwargs):
+
+        super(ArcMarginProduct, self).__init__(**kwargs)
+        self.n_classes = n_classes
+        self.s = s
+        self.m = m
+        self.ls_eps = ls_eps
+        self.easy_margin = easy_margin
+        self.cos_m = tf.math.cos(m)
+        self.sin_m = tf.math.sin(m)
+        self.th = tf.math.cos(math.pi - m)
+        self.mm = tf.math.sin(math.pi - m) * m
+
+    def get_config(self):
+
+        config = super().get_config().copy()
+        config.update({
+            'n_classes': self.n_classes,
+            's': self.s,
+            'm': self.m,
+            'ls_eps': self.ls_eps,
+            'easy_margin': self.easy_margin,
+        })
+        return config
+
+    def build(self, input_shape):
+        super(ArcMarginProduct, self).build(input_shape[0])
+
+        self.W = self.add_weight(
+            name='W',
+            shape=(int(input_shape[0][-1]), self.n_classes),
+            initializer='glorot_uniform',
+            dtype='float32',
+            trainable=True,
+            regularizer=None)
+
+    def call(self, inputs):
+        X, y = inputs
+        y = tf.cast(y, dtype=tf.int32)
+        cosine = tf.matmul(
+            tf.math.l2_normalize(X, axis=1),
+            tf.math.l2_normalize(self.W, axis=0)
+        )
+        sine = tf.math.sqrt(1.0 - tf.math.pow(cosine, 2))
+        phi = cosine * self.cos_m - sine * self.sin_m
+        if self.easy_margin:
+            phi = tf.where(cosine > 0, phi, cosine)
+        else:
+            phi = tf.where(cosine > self.th, phi, cosine - self.mm)
+        one_hot = tf.cast(
+            tf.one_hot(y, depth=self.n_classes),
+            dtype=cosine.dtype
+        )
+        if self.ls_eps > 0:
+            one_hot = (1 - self.ls_eps) * one_hot + self.ls_eps / self.n_classes
+
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output *= self.s
+        return output
