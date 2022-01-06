84b1386c6155dc7eac7192f66c805e613774ea9b
Jiyeon Lee <i3048i@yonsei.ac.kr>
2022-01-06 17:30:54 +0900

    Update streamlit_test.py

--- etc/streamlit_test.py
+++ etc/streamlit_test.py
@@ -20,30 +20,30 @@ if st.button('Enter'):
     tmp = df[df.ctlg_no == int(ctlg_no)]
     info = tmp.iloc[0]
     dic = {
-        'ds' : ast.literal_eval(info['train_ds']),
-        'y' : np.square(ast.literal_eval(info['train_y'])),
-        'avg_prc' : ast.literal_eval(info['train_avg_prc'])
+        '날짜' : ast.literal_eval(info['train_ds']),
+        '판매량' : np.square(ast.literal_eval(info['train_y'])),
+        '가격' : ast.literal_eval(info['train_avg_prc'])
     }
     
     result = pd.DataFrame(dic)
-    result['ds'] = pd.to_datetime(result['ds'])
+    result['날짜'] = pd.to_datetime(result['날짜'])
     st.dataframe(result)
     
     st.write("### 일자별 판매량 - 가격 그래프")
     fig, ax = plt.subplots()
-    ax.plot(result['ds'], result['y'], label='sales', ls='-')
-    ax.set_ylabel('sales')
+    ax.plot(result['날짜'], result['판매량'], label='sales', ls='-')
+    ax.set_ylabel('판매량')
     plt.legend()
     ax2 = ax.twinx()
-    ax2.set_ylabel('price')
-    plt.plot(result['ds'], result['avg_prc'], color='deeppink', label='price')
+    ax2.set_ylabel('가격')
+    plt.plot(result['날짜'], result['가격'], color='deeppink', label='price')
     plt.legend()
     
-    st.write("파란색 선 : sales, 분홍색 선 : price")
+    st.write("파란색 선 : 판매량, 분홍색 선 : 가격")
     st.pyplot(fig)
     
     st.write("### 가격 - 판매량 관계 그래프")
-    tmp2 = result.groupby('avg_prc').sum('y')
+    tmp2 = result.groupby('가격').sum('판매량')
     st.dataframe(tmp2)
     st.line_chart(tmp2)
     
