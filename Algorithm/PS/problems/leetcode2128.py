e1add6dd15b2b9c16b678356c722129dab9ce101
이지연(Jiyeon Lee)/Machine Intelligence팀/11ST <32065860+jiyeoon@users.noreply.github.com>
2022-03-18 23:42:18 +0900

    Create leetcode2128.py

---
+++ Algorithm/PS/problems/leetcode2128.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeOnes(self, grid: List[List[int]]) -> bool:
+        r1, r1_invert = grid[0], [1-val for val in grid[0]]
+        for i in range(1, len(grid)):
+            if grid[i] != r1 and grid[i] != r1_invert:
+                return False
+        return True
