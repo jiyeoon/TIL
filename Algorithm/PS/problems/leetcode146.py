1e2a089fc7880725fb03d6915c07756d09a5e418
이지연(Jiyeon Lee)/Machine Intelligence팀/11ST <32065860+jiyeoon@users.noreply.github.com>
2022-03-29 23:00:21 +0900

    Create leetcode146.py

---
+++ Algorithm/PS/problems/leetcode146.py
@@ -0,0 +1,25 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.dic = collections.OrderedDict()
+        self.remain = capacity
+        
+
+    def get(self, key: int) -> int:
+        if key not in self.dic:
+            return -1
+        value = self.dic.pop(key)
+        self.dic[key] = value
+        return value
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            self.dic.pop(key)
+        else:
+            if self.remain > 0:
+                self.remain -= 1
+            else:
+                self.dic.popitem(last=False)
+        self.dic[key] = value
+        
